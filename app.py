caaa36b7761463ddfd207933198057a2ccaf95da
SabbyDude <75109982+SabbyDude@users.noreply.github.com>
2021-07-27 14:11:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+from flask import  Flask, render_template,request
 from backEnd import backEnd
 
 app= Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return render_template('home.html')
+
+
 
 @app.route('/predict', methods=['POST'])
 def home():
@@ -20,7 +28,7 @@ def home():
     back = backEnd(screenSize, screenRes, Cpu, RAM, weight, touchScreen, HDD, SSD, SSHD, FStorage, Type, Os)
     final_predicted = (round(float(back.totalPredicted[0]),2))
     
-    return render_template('predicted.html', prediction_text = 'Predicted Laptop Price is ₹{} (INR)'.format(final_predicted))
+    return render_template('predicted.html', prediction_text = 'Predicted Laptop Price is €{} (EUR)'.format(final_predicted))
 
 if __name__ == "__main__":
     app.run(debug=True)
